8a1af568f84943755cf1590e0fbc01f40ef59aec
Ori Livneh <ori@wikimedia.org>
2013-01-02 02:24:29 -0800

    Fix-up: unicode handling in setsockopt
    Change-Id: Ib79c9a6ef8efb95ec0f5551fb328b940afe1c653

--- server/eventlogging/stream.py
+++ server/eventlogging/stream.py
@@ -26,9 +26,9 @@ def zmq_subscribe(endpoint, topic='', sid=None, json=False):
     context = zmq.Context.instance()
     sock = context.socket(zmq.SUB)
     if sid is not None:
-        socket.setsockopt(zmq.IDENTITY, sid.encode('utf8'))
+        sock.setsockopt(zmq.IDENTITY, sid.encode('utf8'))
     sock.connect(endpoint)
-    sock.setsockopt_string(zmq.SUBSCRIBE, topic)
+    sock.setsockopt(zmq.SUBSCRIBE, topic.encode('utf8'))
     recv = sock.recv_json if json else sock.recv_unicode
 
     while 1:
