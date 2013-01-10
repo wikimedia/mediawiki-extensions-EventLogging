c0d220fc9b4c4ffd56e940bfb4783fdc0d21f91d
Ori Livneh <ori@wikimedia.org>
2013-01-10 10:03:46 -0800

    Dispatch correct event parser by sniffing content
    This is a hack to see us through the deployment of the change from
    query-encoded event objects to percent-encoded JSON. Let's merge this
    unless there's something serious wrong with it. I will clean it up after
    the deployment tonight.
    
    Change-Id: Ica687893abe015b73a78e06a52f976142d74f7d4

--- server/eventlogging/compat.py
+++ server/eventlogging/compat.py
@@ -9,7 +9,7 @@
   :license: GNU General Public Licence 2.0 or later
 
 """
-__all__ = ('items', 'json', 'parse_qsl', 'urlopen')
+__all__ = ('items', 'json', 'parse_qsl', 'urlopen', 'iter_socket', 'unquote')
 
 import operator
 import sys
@@ -20,12 +20,12 @@ PY3 = sys.version_info[0] == 3
 
 if PY3:
     items = operator.methodcaller('items')
-    from urllib.parse import parse_qsl
+    from urllib.parse import parse_qsl, unquote
     from urllib.request import urlopen
 else:
     items = operator.methodcaller('iteritems')
     from urlparse import parse_qsl
-    from urllib2 import urlopen
+    from urllib2 import urlopen, unquote
 
 
 def iter_socket(sock, encoding='utf8', errors='replace'):
