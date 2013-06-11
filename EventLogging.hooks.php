ba7306bc4a85454aa29039f165b08a5911cc4765
Matthew Flaschen <mflaschen@wikimedia.org>
2013-06-11 18:51:43 -0400

    Move PageContentSaveComplete out; intended to be in CoreEvents
    Change-Id: I2fafa3e5325bdfabbffc74f4691c839fb44c6742

--- EventLogging.hooks.php
+++ EventLogging.hooks.php
@@ -36,24 +36,6 @@ class EventLoggingHooks {
 	}
 
 	/**
-	 * Log server-side event on successful page edit.
-	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/PageContentSaveComplete
-	 * @see https://meta.wikimedia.org/wiki/Schema:PageContentSaveComplete
-	 */
-	public static function onPageContentSaveComplete( $article, $user, $content, $summary,
-		$isMinor, $isWatch, $section, $flags, $revision, $status, $baseRevId ) {
-
-		if ( $revision ) {
-			$event = array( 'revisionId' => $revision->getId() );
-			if ( isset( $_SERVER[ 'HTTP_USER_AGENT' ] ) ) {
-				$event[ 'userAgent' ] = $_SERVER[ 'HTTP_USER_AGENT' ];
-			}
-			efLogServerSideEvent( 'PageContentSaveComplete', 5303086, $event );
-		}
-		return true;
-	}
-
-	/**
 	 * @param array &$vars
 	 * @return bool
 	 */
