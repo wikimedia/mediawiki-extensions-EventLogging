18b7693275ec6d693c6536f09293455191aa8e5a
Ori Livneh <ori@wikimedia.org>
2014-11-22 22:10:31 -0800

    Make PeriodicThread more regular
    Right now, PeriodicThread will sleep the full duration of the interval
    regardless of how long the target function takes to complete. This can make the
    queue back up precipitously during high load. Mitigate this by subtracting the
    run-time of the target function from the sleep interval.
    
    Change-Id: Ib746a2571ea25058de49caeae9aad83ec4c32157

--- server/eventlogging/utils.py
+++ server/eventlogging/utils.py
@@ -12,12 +12,14 @@ from __future__ import unicode_literals
 import re
 import threading
 
+from .compat import monotonic_clock
+
 
 __all__ = ('PeriodicThread', 'uri_delete_query_item')
 
 
 class PeriodicThread(threading.Thread):
-    """Represents a threaded job that runs repeatedly at regular intervals."""
+    """Represents a threaded job that runs repeatedly at a regular interval."""
 
     def __init__(self, interval, *args, **kwargs):
         self.interval = interval
@@ -27,12 +29,24 @@ class PeriodicThread(threading.Thread):
 
     def run(self):
         while not self.stopping.is_set():
-            if self.ready.wait(self.interval):
+            # Run the target function. Check the clock before
+            # and after to determine how long it took to run.
+            time_start = monotonic_clock()
+            self._Thread__target(*self._Thread__args, **self._Thread__kwargs)
+            time_stop = monotonic_clock()
+
+            run_duration = time_stop - time_start
+
+            # Subtract the time it took the target function to run
+            # from the desired run interval. The result is how long
+            # we have to sleep before the next run.
+            time_to_next_run = self.interval - run_duration
+
+            if self.ready.wait(time_to_next_run):
                 # If the internal flag of `self.ready` was set, we were
                 # interrupted mid-nap to run immediately. But before we
                 # do, we reset the flag.
                 self.ready.clear()
-            self._Thread__target(*self._Thread__args, **self._Thread__kwargs)
 
     def stop(self):
         """Graceful stop: stop once the current iteration is complete."""
