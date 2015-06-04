40530d137ff4e06027994c1f3c648391e189a4e5
Andrew Otto <acotto@gmail.com>
2015-06-04 14:47:12 -0700

    Allow Kafka writers topic and message keys to be configured using event information
    This allow a single eventlogging-consumer to effectively multiplex the incoming event stream
    to multiple Kafka topics.  This is now a valid kafka URI:
    
      'kafka://?brokers=analytics1021.eqiad.wmnet:9092&topic=eventlogging-%(schema)s'
    
    Change-Id: Ia49f3da7bc256542dd1068538c4c031b6933efd9

--- server/eventlogging/handlers.py
+++ server/eventlogging/handlers.py
@@ -22,6 +22,7 @@ import os
 import socket
 import sys
 import sqlalchemy
+import time
 import traceback
 
 from .utils import PeriodicThread, uri_delete_query_item
@@ -68,27 +69,87 @@ def mongodb_writer(uri, database='events'):
 
 
 @writes('kafka')
-def kafka_writer(brokers, topic='eventlogging', raw=False, key=''):
-    """Write events to Kafka.
-       Set key to SCID if events are not raw and key is not set ."""
+def kafka_writer(
+    brokers,
+    topic='eventlogging-%(schema)s',
+    key='%(schema)s_%(revision)s',
+    raw=False,
+):
+    """
+    Write events to Kafka.
+
+        brokers - Should be a list of Kafka Brokers.
+
+        topic   - A static of formatted string topic name.
+                  If the incoming event is a dict (not a raw string)
+                  topic will be interpolated against event.  I.e.
+                  topic % event.  Default: eventlogging
+
+        key     - Key of the event message in Kafka.
+                  If the incoming event is a dict (not a raw string)
+                  key will be interpolated against event.  I.e.
+                  key % event.  Default: '%(schema)s_%(revision)s'
+
+        raw     - Should the events be written as raw (encoded) or not?
+    """
     from kafka import KafkaClient
     from kafka import KeyedProducer
+    from kafka.common import KafkaTimeoutError
 
     kafka = KafkaClient(brokers)
     producer = KeyedProducer(kafka)
-    topic = topic.encode('utf-8')
-    key = key.encode('utf-8')
+
+    # These will be used if incoming events are not interpolatable.
+    default_topic = topic.encode('utf8')
+    default_key = key.encode('utf8')
+
+    kafka_topic_create_timeout_seconds = 0.1
+
     while 1:
         event = (yield)
+
+        # If event is a dict (not Raw) then we can interpolate topic and key
+        # as format strings.  E.g. topic = 'eventlogging_%(schema)' % event.
+        if isinstance(event, dict):
+            message_topic = (topic % event).encode('utf8')
+            message_key = (key % event).encode('utf8')
+        else:
+            message_topic = default_topic
+            message_key = default_key
+
+        try:
+            # Make sure this topic exists before we attempt to produce to it.
+            # This call will timeout in kafka_topic_create_timeout_seconds.
+            # This should return faster than this if this kafka client has
+            # already cached topic metadata for this topic.  Otherwise
+            # it will try to ask Kafka for it each time.  Make sure
+            # auto.create.topics.enabled is true for your Kafka cluster!
+            kafka.ensure_topic_exists(
+                topic,
+                kafka_topic_create_timeout_seconds
+            )
+        except KafkaTimeoutError:
+            error_message = "Failed to ensure Kafka topic %s exists " \
+                "in %f seconds when producing event" % (
+                    topic,
+                    kafka_topic_create_timeout_seconds
+                )
+            if isinstance(event, dict):
+                error_message += " of schema %s revision %d" % (
+                    event['schema'],
+                    event['revision']
+                )
+            error_message +=
+                ". Skipping event. (This might be ok if this is a new topic.)"
+            logging.error(error_message)
+            continue
+
         if raw:
             value = event.encode('utf-8')
         else:
-            if not key:
-                # set key to SCID, for instance 'EchoMail_5467650'
-                key = '%(schema)s_%(revision)s' % event
-                key = key.encode('utf-8')
             value = json.dumps(event, sort_keys=True)
-        producer.send(topic, key, value)
+
+        producer.send(message_topic, message_key, value)
 
 
 @writes('mysql', 'sqlite')
