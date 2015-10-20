a7fc561ee9ee925946a85c432d01b2ce8187c7b2
Timo Tijhof <krinklemail@gmail.com>
2015-10-20 23:00:33 +0100

    ext.eventLogging.subscriber: Minor code clean up
    * Use local 'mw' instead of global 'mediaWiki' variable.
    
    * Use simple [i] syntax instead of charAt(). This code no longer
      has to support IE6.
    
    * Fix JSDuck syntax (un-index file-level comment block, and
      user lowercase for primitive types).
    
    Change-Id: I558438045654646a220657e5c506aadd61f458e3

--- modules/ext.eventLogging.subscriber.js
+++ modules/ext.eventLogging.subscriber.js
@@ -1,4 +1,4 @@
-/**
+/*!
  * This module registers an EventLogging handler for 'event'-namespaced
  * events logged via mw#track. The handler ensures that the appropriate
  * schema module and core EventLogging code are loaded, and then it logs
@@ -17,24 +17,24 @@
 	/**
 	 * Convert the first letter of a string to uppercase.
 	 *
-	 * @param {String} word
-	 * @return {String}
+	 * @param {string} word
+	 * @return {string}
 	 */
 	function titleCase( word ) {
-		return word.charAt( 0 ).toUpperCase() + word.slice( 1 );
+		return word[ 0 ].toUpperCase() + word.slice( 1 );
 	}
 
 	/**
 	 * mw#track handler for EventLogging events.
 	 *
-	 * @param {String} topic Topic name ('event.*').
+	 * @param {string} topic Topic name ('event.*').
 	 * @param {Object} event
 	 */
 	function handleTrackedEvent( topic, event ) {
 		var schema = titleCase( topic.slice( topic.indexOf( '.' ) + 1 ) ),
 			dependencies = [ 'ext.eventLogging', 'schema.' + schema ];
 
-		mediaWiki.loader.using( dependencies, function () {
+		mw.loader.using( dependencies, function () {
 			mw.eventLog.logEvent( schema, event );
 		} );
 	}
