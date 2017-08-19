9b56517f9795d6d3ebb7f0260868dde09e879309
Umherirrender <umherirrender_de.wp@web.de>
2017-08-19 09:44:46 +0200

    Use json extension for .stylelintrc
    Bug: T173516
    Change-Id: Ia0221ec51d2352756e8884cf0b2aca85d6f5b3c6

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,14 +22,15 @@ module.exports = function ( grunt ) {
 		jsonlint: {
 			all: [
 				'**/*.json',
-				'.stylelintrc',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!vendor/**'
 			]
 		},
 		stylelint: {
 			all: [
 				'**/*.css',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!vendor/**'
 			]
 		},
 		banana: {
