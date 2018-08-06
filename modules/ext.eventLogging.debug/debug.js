93891f8d35dbea58e34e48c7fdba7a5428af6d08
Timo Tijhof <krinklemail@gmail.com>
2018-08-06 18:40:44 +0100

    debug: Minor clean up
    * Use 'long' autoHide instead of the default 'short'.
      This increases visibility of the notification from 5s to 30s.
    
    * Use console.info(), if available, instead of mw.log(), because
      mw.log() is disabled by default and only replaced with a proxy
      for console.log in RL debug mode.
    
      This way, the text is always in the console after enabling
      EL debug mode, regardless of RL debug mode.
    
      Also, log the inspectable event object, instead of its JSON.
    
    * Fix dialog 'large' size option.
    
      The call to setSize() didn't work, causing the dialog
      to open at the default size of 'small' instead.  (see T201345).
      Use the 'size' option to openWindow(), which does work.
    
    * Remove loading of deprecated/no-op 'json' module, and
      add preload for 'oojs-ui-windows' in the same batch.
    
    Bug: T187207
    Change-Id: I095a9b55b4f2703d3469e1a6881a19f7c85cd0a5

--- modules/ext.eventLogging.debug/debug.js
+++ modules/ext.eventLogging.debug/debug.js
@@ -1,5 +1,5 @@
-/*!
- * Inspect events on page views.
+/**
+ * EventLogging client-side debug mode: Inspect events on page views.
  *
  * To enable, run the following from the browser console:
  *
@@ -14,67 +14,84 @@
  *     } );
  *
  * See EventLoggingHooks.php for the module loading, and user option registation.
+ *
+ * @private
+ * @class mw.eventLog.Debug
+ * @singleton
  */
 ( function ( mw, $ ) {
 	'use stict';
 
 	var dialogPromise;
 
-	function initDialogPromise() {
-		return mw.loader.using( 'oojs-ui-windows' )
-			.then( function () {
-				/* global OO */
-				var wm = new OO.ui.WindowManager(),
-					dialog = new OO.ui.MessageDialog();
+	/**
+	 * @private
+	 * @return {jQuery.Promise} Yields a function to open an OOUI Window
+	 */
+	function makeDialogPromise() {
+		return mw.loader.using( 'oojs-ui-windows' ).then( function () {
+			/* global OO */
+			var manager = new OO.ui.WindowManager(),
+				dialog = new OO.ui.MessageDialog();
+			$( 'body' ).append( manager.$element );
+			manager.addWindows( [ dialog ] );
 
-				wm.addWindows( [ dialog ] );
-				dialog.setSize( 'large' );
-				$( 'body' ).append( wm.$element );
+			return function openDialog( args ) {
+				manager.openWindow( dialog, $.extend( {
+					verbose: true,
+					size: 'large',
+					actions: [
+						{
+							action: 'accept',
+							label: mw.msg( 'ooui-dialog-message-accept' ),
+							flags: 'primary'
+						}
+					]
+				}, args ) );
+			};
+		} );
+	}
 
-				return function ( args ) {
-					wm.openWindow( dialog, $.extend( {
-						verbose: true,
-						actions: [
-							{
-								action: 'accept',
-								label: mw.msg( 'ooui-dialog-message-accept' ),
-								flags: 'primary'
-							}
-						]
-					}, args ) );
-				};
+	/**
+	 * @private
+	 * @param {Object} event As formatted by mw.eventLog.prepare()
+	 */
+	function displayLoggedEvent( event ) {
+		var baseUrl = mw.config.get( 'wgEventLoggingSchemaApiUri' ).replace( 'api.php', 'index.php' ),
+			json = JSON.stringify( event, null, 2 ),
+			formatted = mw.format(
+				mw.html.escape( 'Log event ($1): $2' ),
+				mw.html.element( 'a',
+					{ href: baseUrl + '?oldid=' + event.revision },
+					'Schema: ' + event.schema
+				),
+				mw.html.element( 'tt', {},
+					JSON.stringify( event.event, null, 1 ).slice( 0, 100 ) + '...'
+				)
+			),
+			content = $( '<p>' ).html( formatted );
+
+		content.on( 'click', function () {
+			dialogPromise = dialogPromise || makeDialogPromise();
+			dialogPromise.then( function ( openDialog ) {
+				openDialog( {
+					title: 'Schema: ' + event.schema,
+					message: $( '<pre>' ).text( json )
+				} );
 			} );
+		} );
+
+		/* eslint-disable no-console */
+		if ( window.console && console.info ) {
+			console.info( event.schema, event );
+		}
+		mw.notification.notify( content, { autoHide: true, autoHideSeconds: 'long' } );
 	}
 
 	mw.trackSubscribe( 'eventlogging.debug', function ( topic, event ) {
-		mw.loader.using( [ 'json', 'mediawiki.notification' ] )
-			.then( function () {
-				var baseUrl = mw.config.get( 'wgEventLoggingSchemaApiUri' ).replace( 'api.php', 'index.php' ),
-					json = JSON.stringify( event, null, 2 ),
-					formatted = mw.format(
-						mw.html.escape( 'Log event ($1): $2' ),
-						mw.html.element( 'a',
-							{ href: baseUrl + '?oldid=' + event.revision },
-							'Schema: ' + event.schema
-						),
-						mw.html.element( 'tt', {},
-							JSON.stringify( event.event, null, 1 ).slice( 0, 100 ) + '...'
-						)
-					),
-					content = $( '<p>' ).html( formatted );
-
-				content.on( 'click', function () {
-					dialogPromise = dialogPromise || initDialogPromise();
-					dialogPromise.then( function ( openDialog ) {
-						openDialog( {
-							title: 'Schema: ' + event.schema,
-							message: $( '<pre>' ).text( json )
-						} );
-					} );
-				} );
-
-				mw.log( json );
-				mw.notification.notify( content, { autoHide: true } );
-			} );
+		mw.loader.using( [ 'mediawiki.notification', 'oojs-ui-windows' ] ).then( function () {
+			displayLoggedEvent( event );
+		} );
 	} );
+
 }( mediaWiki, jQuery ) );
