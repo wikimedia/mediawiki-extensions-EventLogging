03da602ec7ac8b75d24f48cba9f5e5414532b21c
Fomafix <fomafix@googlemail.com>
2018-09-07 15:03:02 +0200

    Add requirement to "MediaWiki": ">= 1.32.0" in extension.json
    20449aec depends on 65b6faa5 from core included in MediaWiki 1.32.
    
    Also start showing warning for deprecated PHP entry point.
    
    Change-Id: Ic35582da3fbc4ec19005aa35698474406599bde1

--- EventLogging.php
+++ EventLogging.php
@@ -6,12 +6,12 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	$wgMessagesDirs['EventLogging'] = __DIR__ . '/i18n/core';
 	$wgMessagesDirs['JsonSchema'] = __DIR__ . '/i18n/jsonschema';
 	$wgExtensionMessagesFiles['EventLoggingNamespaces'] = __DIR__ . '/EventLogging.namespaces.php';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for EventLogging extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
-	die( 'This version of the EventLogging extension requires MediaWiki 1.25+' );
+	die( 'This version of the EventLogging extension requires MediaWiki 1.32+' );
 }
