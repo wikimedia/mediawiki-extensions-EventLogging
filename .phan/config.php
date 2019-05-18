790f65235b50f5af68523840c06b96c514bf5a12
Umherirrender <umherirrender_de.wp@web.de>
2019-05-18 18:10:01 +0200

    build: Update mediawiki/mediawiki-phan-config to 0.6.0
    Remove the now included stub folder from config
    
    Change-Id: I5e44cf38be1b776d2c8dfef7f3594903a8dfaa45

--- .phan/config.php
+++ .phan/config.php
@@ -4,11 +4,7 @@ $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.
 
 $cfg['file_list'][] = 'EventLogging.namespaces.php';
 
-// TODO Remove on new version of phan-config
-$cfg['directory_list'][] = '.phan/stubs/';
-$cfg['exclude_analysis_directory_list'][] = '.phan/stubs/';
-
-// Phan now includes dev dependencies - hopefully will be removed
+// T223397 - Phan now includes dev dependencies
 $cfg['suppress_issue_types'][] = 'PhanRedefinedInheritedInterface';
 
 // T191666
