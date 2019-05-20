5fe517f9f441b94cce782f1e66086f993ec1cc3a
Umherirrender <umherirrender_de.wp@web.de>
2019-05-20 17:45:23 +0200

    build: Remove PhanRedefinedInheritedInterface suppression
    If7171a6d46473b0bb05e3adaeb40229e7881c2b6 adjusts the CI configuration
    to no more install MediaWiki core development dependencies and solely
    relies on mediawiki/vendor.git
    
    Bug: T223397
    Depends-On: If7171a6d46473b0bb05e3adaeb40229e7881c2b6
    Change-Id: I09fd30217bd53334c0131608c64de610defe6e1b

--- .phan/config.php
+++ .phan/config.php
@@ -4,9 +4,6 @@ $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.
 
 $cfg['file_list'][] = 'EventLogging.namespaces.php';
 
-// T223397 - Phan now includes dev dependencies
-$cfg['suppress_issue_types'][] = 'PhanRedefinedInheritedInterface';
-
 // T191666
 $cfg['suppress_issue_types'][] = 'PhanParamTooMany';
 
