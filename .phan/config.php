189d7fc61c452853afa1320addc3e11372573a13
Umherirrender <umherirrender_de.wp@web.de>
2019-10-04 11:24:34 +0200

    phan: Remove suppression for PhanParamTooMany
    Depends-On: I59f2ae1a96af392026fc106e57d23553003c16b8
    Change-Id: Ie542f6b669e9cbd6e384bc42cb5c6e258170f479

--- .phan/config.php
+++ .phan/config.php
@@ -4,7 +4,4 @@ $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.
 
 $cfg['file_list'][] = 'EventLogging.namespaces.php';
 
-// T191666
-$cfg['suppress_issue_types'][] = 'PhanParamTooMany';
-
 return $cfg;
