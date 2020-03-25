5576c8e41b65746552202db59db6c6fe48cd8aeb
Timo Tijhof <krinklemail@gmail.com>
2020-03-25 01:12:10 +0000

    tests: Cover unknown stream in streamConfig()
    Add test case for unknown stream passed to streamConfig returning
    null.
    
    Also a few minor simplifications to make the tests a bit stricter
    and easier to debug:
    
    * Avoid assert.ok(), use an explicit comparison instead by stopping
      the clock and performing ticks before and after the observed call.
    
    * Avoid nesting assertions or done() in callbacks we don't own (e.g. anything
      other than inline Promise chains or assert.reject callbacks). Such nested
      assertions can result in timeouts or accidental success if the callback
      isn't called. Instead, after the work is expected to be done, assert
      the result (either by closure, or with a Sinon spy).
    
      This has the added benefit of not being tolerant to unintended
      changes in order, or repeats. For example,
    
        thing.one( x =>  assert.equal(x, 1) );
        thing.two( x =>  assert.equal(x, 2) );
    
      This passes if there are other assertions and thing isn't called.
      It passes if 2 happens before 1.
      It passes if one or both happen multiple times.
    
      Nothing major. These are unlikely edge cases, and might even be allowed.
      But when possible I'd like tests to explicit rule out such variation,
      which this pattern allows :)
    
    Change-Id: I5816578d8b405efef0989f15473bf7383d64e4a4

--- tests/ext.eventLogging/BackgroundQueue.test.js
+++ tests/ext.eventLogging/BackgroundQueue.test.js
@@ -1,16 +1,24 @@
 /* eslint-env qunit */
 'use strict';
 
-QUnit.module( 'ext.eventLogging/BackgroundQueue' );
+QUnit.module( 'ext.eventLogging/BackgroundQueue', {
+	beforeEach: function () {
+		this.clock = this.sandbox.useFakeTimers();
+	}
+} );
 
 QUnit.test( 'add()', function ( assert ) {
 	var q = new mw.eventLog.BackgroundQueue( 1 / 1000 ),
-		done = assert.async();
+		seen = [];
 	q.add( function () {
-		assert.strictEqual( q.getTimer(), null );
-		assert.strictEqual( q.getCallbacks().length, 0 );
-		done();
+		seen.push( 'x' );
 	} );
+	assert.deepEqual( [], seen );
 	assert.strictEqual( typeof q.getTimer(), 'number' );
 	assert.strictEqual( q.getCallbacks().length, 1 );
+
+	this.clock.tick( 1 );
+	assert.deepEqual( [ 'x' ], seen );
+	assert.strictEqual( q.getTimer(), null );
+	assert.strictEqual( q.getCallbacks().length, 0 );
 } );
--- tests/ext.eventLogging/stream.test.js
+++ tests/ext.eventLogging/stream.test.js
@@ -1,46 +1,57 @@
 /* eslint-env qunit */
 'use strict';
 
-QUnit.module( 'ext.eventLogging/stream' );
+QUnit.module( 'ext.eventLogging/stream', {
+	beforeEach: function () {
+		this.clock = this.sandbox.useFakeTimers();
+	}
+} );
 
 QUnit.test( 'submit() - warn for event without schema', function ( assert ) {
-	var done = assert.async();
-	this.sandbox.stub( mw.eventLog, 'enqueue', function () {
-		assert.ok( false, 'enqueue should not be reached' );
-	} );
+	var seen = [];
+	this.sandbox.stub( mw.eventLog, 'enqueue' );
 	this.sandbox.stub( mw.log, 'warn', function () {
-		done();
+		seen.push( 'warn' );
 	} );
-	assert.timeout( 100 );
-	assert.expect( 0 );
 	mw.eventLog.streamConfigs[ 'test.stream' ] = { some: 'config' };
 	mw.eventLog.submit( 'test.stream', {} );
+	assert.deepEqual( [ 'warn' ], seen );
+	assert.strictEqual( mw.eventLog.enqueue.callCount, 0, 'enqueue() calls' );
 } );
 
 QUnit.test( 'submit() - produce an event correctly', function ( assert ) {
-	var t0 = new Date().toISOString(),
-		done = assert.async();
+	var t1, jsonString, data;
 	this.sandbox.stub( mw.eventLog, 'enqueue', function ( callback ) {
-		// Ensure callback is called right away, regardless of BackgroundQueue config.
+		// Stub BackgroundQueue, regardless of intervalSecs config.
 		callback();
 	} );
-	this.sandbox.stub( mw.log, 'warn', function () {
-		assert.ok( false, 'log warn should not be reached' );
-	} );
-	this.sandbox.stub( navigator, 'sendBeacon', function ( uri, jsonString ) {
-		var t1 = new Date().toISOString(),
-			data = JSON.parse( jsonString );
-		assert.ok( data.meta.dt >= t0 && data.meta.dt <= t1, 'dt is valid' );
-		assert.equal( data.meta.stream, 'test.stream', 'stream is valid' );
-		done();
-	} );
+	this.sandbox.stub( mw.log, 'warn' );
+	this.sandbox.stub( navigator, 'sendBeacon' );
+
+	this.clock.tick( 1000 );
+	t1 = new Date().toISOString();
 	mw.eventLog.setOptionsForTest( { serviceUri: 'testUri' } );
 	mw.eventLog.streamConfigs[ 'test.stream' ] = { some: 'config' };
 	mw.eventLog.submit( 'test.stream', { $schema: 'test/schema' } );
+	this.clock.tick( 1000 );
+
+	assert.strictEqual( mw.log.warn.callCount, 0, 'warn() calls' );
+	assert.strictEqual( navigator.sendBeacon.callCount, 1, 'sendBeacon() calls' );
+	jsonString = navigator.sendBeacon.args[ 0 ][ 1 ];
+	data = JSON.parse( jsonString );
+	assert.strictEqual( data.meta.dt, t1, 'dt is valid' );
+	assert.strictEqual( data.meta.stream, 'test.stream', 'stream is valid' );
 } );
 
 QUnit.test( 'streamConfig() - disallow modification', function ( assert ) {
 	mw.eventLog.streamConfigs[ 'test.stream' ] = { field: 'expectedValue' };
 	mw.eventLog.streamConfig( 'test.stream' ).field = 'otherValue';
 	assert.equal( mw.eventLog.streamConfigs[ 'test.stream' ].field, 'expectedValue' );
+} );
+
+QUnit.test( 'streamConfig() - unknown stream', function ( assert ) {
+	assert.strictEqual(
+		mw.eventLog.streamConfig( 'test.unknown' ),
+		null
+	);
 } );
