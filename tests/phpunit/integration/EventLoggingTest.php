9e867c2fc0ff4407361173391a44f27fb93e8e7c
Ottomata <aotto@wikimedia.org>
2021-01-13 16:05:39 -0500

    Refactor EventLogging Event Platform PHP integration
    Fixes:
    - 'event' data via EventLogging::logEvent is now properly
      set in the 'event' field, not top level.
    
    - Removed useless PHP schema validation; a schema with
      clientValidated set would not validate anywhere anyway.
    
    - Deprecated EventLogging::logEvent $options parameter.
      According to codesearch.wmcloud.org, this is not used,
      and removing it is simpler than supporting it in
      EventServiceClient too.
    
    - EventLogging::encapsulate now handles encapsulating
      legacy events for use either by old eventlogging backend
      or by new Event Platform schemas.
    
    - EventServiceClient::prepareEvent sets fields that should be
      set on all new Event Platform based events, including those
      'legacy' EventLogging ones that have been migrated off of metawiki.
    
    This makes the functions in EventLogging class closer to
    those in the JS code.  It also reduces the number of
    places the $event can be modified.  EventLogging now only
    modifies (and encapsulates) the event in EventLogging::encapsulate,
    which is used to prepare a legacy event for EITHER POSTing to
    EventGate (if migrated) or sendBeacon-ing to the deprecated
    eventlogging backend.
    
    The event is also modified by EventServiceClient::prepareEvent.
    
    Bug: T253121
    Change-Id: I0cfd07295d4b51ca4805c79a28a9a4bd64ff7cc9

--- tests/phpunit/integration/EventLoggingTest.php
+++ tests/phpunit/integration/EventLoggingTest.php
@@ -11,7 +11,39 @@ class EventLoggingTest extends MediaWikiIntegrationTestCase {
 	private $mockHttpRequestFactory;
 	private $timestamp;
 
+	/**
+	 * This is supposed to be what a user might provide to EventLogging::logEvent
+	 * i.e. an un-encapsulated event.  Used in tests as input data.
+	 * @var array
+	 */
+	private $legacyEvent = [
+		"field_a" => "hi"
+	];
+
+	/*
+	 * HTTP_HOST will be set to this value during the tests if it isn't already set.
+	 * @var string
+	 */
+	private $testHttpHost = 'test.eventlogging.domain';
+
+	/**
+	 * If true, HTTP_HOST has been modified and should be reset during tearDown.
+	 * @var boolean
+	 */
+	private $modifiedHttpHost = false;
+
 	protected function setUp(): void {
+		parent::setUp();
+
+		// EventLoggging uses HTTP_HOST.  If it isn't set for tests, set a dummy,
+		// Otherwise use the actual value.
+		if ( !isset( $_SERVER['HTTP_HOST'] ) ) {
+			$this->modifiedHttpHost = true;
+			$_SERVER['HTTP_HOST'] = $this->testHttpHost;
+		} else {
+			$this->testHttpHost = $_SERVER['HTTP_HOST'];
+		}
+
 		$this->mockEventServiceClient = $this->createMock( EventServiceClient::class );
 		$this->setService( 'EventServiceClient', function () {
 			return $this->mockEventServiceClient;
@@ -29,8 +61,18 @@ class EventLoggingTest extends MediaWikiIntegrationTestCase {
 				'Migrated' => '/test/event/1.0.0',
 				'NotMigrated' => 1337,
 			],
-			'wgEventLoggingBaseUri' => 'https://test.wikipedia.org/beacon/event'
+			'wgEventLoggingBaseUri' => 'https://test.wikipedia.org/beacon/event',
+			'wgServerName' => $this->testHttpHost,
 		] );
+	}
+
+	protected function tearDown(): void {
+		parent::tearDown();
+
+		// IF setHttpHost in setUp, we should unset it in tearDown.
+		if ( $this->modifiedHttpHost ) {
+			unset( $_SERVER['HTTP_HOST'] );
+		}
 	}
 
 	public function testSendMigratedEventLoggingEvent(): void {
@@ -40,12 +82,13 @@ class EventLoggingTest extends MediaWikiIntegrationTestCase {
 				return (
 					$event['$schema'] === '/test/event/1.0.0' &&
 					preg_match( $this->timestamp->regexes['TS_ISO_8601'], $event['client_dt'] ) &&
-					is_string( $event['meta']['domain'] ) &&
-					is_string( $event['http']['request_headers']['user-agent'] )
+					// $eventData['event'] should equal $legacyEvent
+					$event['event']['field_a'] === $this->legacyEvent['field_a']
 				);
 			} ) );
 		$this->mockHttpRequestFactory->expects( $this->never() )->method( 'post' );
-		EventLogging::logEvent( 'Migrated', 1337, [] );
+
+		EventLogging::logEvent( 'Migrated', 1337,  $this->legacyEvent );
 	}
 
 	public function testSendNonMigratedEventLoggingEvent(): void {
@@ -55,20 +98,27 @@ class EventLoggingTest extends MediaWikiIntegrationTestCase {
 			->with( $this->callback( function ( $url ) {
 				return (
 					is_string( $url ) &&
-					str_starts_with( $url, 'https://test.wikipedia.org/beacon/event?' )
+					str_starts_with( $url, 'https://test.wikipedia.org/beacon/event?' ) &&
+					// Test that the $legacyEvent is in the encoded event.
+					// "field_a": "hi" url encodes to this.
+					str_contains( $url, "%22field_a%22%3A%22hi%22" )
 				);
 			} ) );
-		EventLogging::logEvent( 'NotMigrated', 1337, [] );
+
+		EventLogging::logEvent( 'NotMigrated', 1337,  $this->legacyEvent );
 	}
 
-	public function testPrepareEventServiceEvent(): void {
-		$result = TestingAccessWrapper::newFromClass( EventLogging::class )
-			->prepareEventServiceEvent( 'test.wikipedia.org', '/test/event/1.0.0', false, [] );
+	public function testEncapulateEventServiceEvent(): void {
+		$result = TestingAccessWrapper::newFromClass( EventLogging::class )->encapsulate(
+			'Migrated',
+			'/test/event/1.0.0',
+			$this->legacyEvent
+		);
+
 		$this->assertSame( $result['$schema'], '/test/event/1.0.0' );
 		$this->assertRegExp( $this->timestamp->regexes['TS_ISO_8601'], $result['client_dt'] );
-		$this->assertSame( 'test.wikipedia.org', $result['meta']['domain'] );
-		$this->assertSame( EventLogging::USER_AGENT_OMITTED,
-			$result['http']['request_headers']['user-agent'] );
+		$this->assertSame( $this->testHttpHost, $result['webHost'] );
+		$this->assertSame( $result['event']['field_a'],  $this->legacyEvent['field_a'] );
 	}
 
 	public function testGetLegacyStreamName(): void {
