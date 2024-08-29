fb471b874c7f2cef45cd3898e89e043f39cedb42
James D. Forrester <jforrester@wikimedia.org>
2024-08-29 13:30:36 -0400

    Replace use of deprecated global URL functions with wfGetUrlUtils()
    This is a quick fix rather than the proper DI fix.
    
    Bug: T319340
    Change-Id: I0e4b6ce5ca45a07205455d4a752cd9227553c61e

--- includes/EventLogging.php
+++ includes/EventLogging.php
@@ -109,7 +109,7 @@ class EventLogging {
 	 */
 	public static function sendBeacon( $url, array $data = [] ) {
 		$fname = __METHOD__;
-		$url = wfExpandUrl( $url, PROTO_INTERNAL );
+		$url = wfGetUrlUtils()->expand( $url, PROTO_INTERNAL ) ?? '';
 		DeferredUpdates::addCallableUpdate( static function () use ( $url, $data, $fname ) {
 			$options = $data ? [ 'postData' => $data ] : [];
 			return MediaWikiServices::getInstance()->getHttpRequestFactory()
