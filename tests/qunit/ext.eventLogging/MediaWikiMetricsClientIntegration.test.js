7f2d88ad92e3f72dd54ddc6f093ae8dfd2957829
jebe <jebe@wikimedia.org>
2024-10-14 17:56:28 +0200

    Create Tests: Add tests for MediaWikiMetricsClientIntegation#isCurrentUserEnrolled()
    The method was introduced in Id388944221c674d6e5a06e092574ea746a030554
    and was fixed soon after in I1fd5ea2e4b374cdd11c900a229765dcfc75f59d7.
    
    Bug: T368326
    Change-Id: Ie6562c552da4a156f7e16a7f7c11088516a7fed9

--- tests/qunit/ext.eventLogging/MediaWikiMetricsClientIntegration.test.js
+++ tests/qunit/ext.eventLogging/MediaWikiMetricsClientIntegration.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-QUnit.module( 'ext.eventLogging/MediaWikiMetricsClientIntegration', {
+QUnit.module( 'ext.eventLogging/MediaWikiMetricsClientIntegration - getCurrentUserExperiments', {
 	beforeEach: function () {
 		this.originalUserExperiments = mw.config.get( 'wgMetricsPlatformUserExperiments' );
 		this.integration = new mw.eventLog.MediaWikiMetricsClientIntegration();
@@ -12,39 +12,36 @@ QUnit.module( 'ext.eventLogging/MediaWikiMetricsClientIntegration', {
 	}
 } );
 
-QUnit.test(
-	'#getCurrentUserExperiments() - null $wgMetricsPlatformUserExperiments',
-	function ( assert ) {
-		mw.config.set( 'wgMetricsPlatformUserExperiments', null );
+// Test cases for getCurrentUserExperiments
+QUnit.test( '#getCurrentUserExperiments() - null $wgMetricsPlatformUserExperiments', function ( assert ) {
+	mw.config.set( 'wgMetricsPlatformUserExperiments', null );
 
-		assert.deepEqual(
-			this.integration.getCurrentUserExperiments(),
-			{
-				experiments: {
-					enrolled: [],
-					assigned: {}
-				}
+	assert.deepEqual(
+		this.integration.getCurrentUserExperiments(),
+		{
+			experiments: {
+				enrolled: [],
+				assigned: {}
 			}
-		);
-	}
-);
+		},
+		'Returns an empty enrolled and assigned object when wgMetricsPlatformUserExperiments is null'
+	);
+} );
 
-QUnit.test(
-	'#getCurrentUserExperiments() - invalid $wgMetricsPlatformUserExperiments',
-	function ( assert ) {
-		mw.config.set( 'wgMetricsPlatformUserExperiments', 'Hello, World!' );
+QUnit.test( '#getCurrentUserExperiments() - invalid $wgMetricsPlatformUserExperiments', function ( assert ) {
+	mw.config.set( 'wgMetricsPlatformUserExperiments', 'Hello, World!' );
 
-		assert.deepEqual(
-			this.integration.getCurrentUserExperiments(),
-			{
-				experiments: {
-					enrolled: [],
-					assigned: {}
-				}
+	assert.deepEqual(
+		this.integration.getCurrentUserExperiments(),
+		{
+			experiments: {
+				enrolled: [],
+				assigned: {}
 			}
-		);
-	}
-);
+		},
+		'Returns an empty enrolled and assigned object when wgMetricsPlatformUserExperiments is invalid'
+	);
+} );
 
 QUnit.test( '#getCurrentUserExperiments()', function ( assert ) {
 	mw.config.set( 'wgMetricsPlatformUserExperiments', {
@@ -62,7 +59,8 @@ QUnit.test( '#getCurrentUserExperiments()', function ( assert ) {
 					foo: 'baz'
 				}
 			}
-		}
+		},
+		'Returns correct enrollment and assignment for valid experiments'
 	);
 } );
 
@@ -82,6 +80,63 @@ QUnit.test( '#getCurrentUserExperiments() - logged-out and temporary users', fun
 				enrolled: [],
 				assigned: {}
 			}
-		}
+		},
+		'Returns an empty enrolled and assigned object when the user is not logged in'
 	);
+} );
+
+// Test cases for isCurrentUserEnrolled
+QUnit.module( 'ext.eventLogging/MediaWikiMetricsClientIntegration - isCurrentUserEnrolled', {
+	beforeEach: function () {
+		this.originalUserExperiments = mw.config.get( 'wgMetricsPlatformUserExperiments' );
+		this.integration = new mw.eventLog.MediaWikiMetricsClientIntegration();
+
+		this.sandbox.stub( mw.user, 'isNamed' ).returns( true );
+		this.sandbox.stub( this.integration, 'getCurrentUserExperiments' );
+	},
+	afterEach: function () {
+		mw.config.set( 'wgMetricsPlatformUserExperiments', this.originalUserExperiments );
+	}
+} );
+
+QUnit.test( 'isCurrentUserEnrolled returns false when user is not logged in', function ( assert ) {
+	mw.user.isNamed.returns( false );
+
+	const result = this.integration.isCurrentUserEnrolled( 'foo' );
+
+	assert.strictEqual( result, false, 'Returns false when the user is not logged in' );
+} );
+
+QUnit.test( 'isCurrentUserEnrolled returns false when user is not enrolled in the experiment', function ( assert ) {
+	mw.user.isNamed.returns( true );
+
+	this.integration.getCurrentUserExperiments.returns( {
+		experiments: {
+			enrolled: [],
+			assigned: {}
+		}
+	} );
+
+	const result = this.integration.isCurrentUserEnrolled( 'foo' );
+
+	assert.strictEqual( result, false, 'Returns false when the user is not enrolled in the experiment' );
+} );
+
+QUnit.test( 'isCurrentUserEnrolled returns true when user is enrolled in the experiment', function ( assert ) {
+	mw.user.isNamed.returns( true );
+
+	this.integration.getCurrentUserExperiments.returns( {
+		experiments: {
+			enrolled: [ 'foo' ],
+			assigned: {
+				foo: true
+			}
+		}
+	} );
+
+	const result = this.integration.isCurrentUserEnrolled( 'foo' );
+
+	assert.strictEqual( result, true, 'Returns true when the user is enrolled in the experiment' );
+
+	this.integration.getCurrentUserExperiments.restore();
 } );
