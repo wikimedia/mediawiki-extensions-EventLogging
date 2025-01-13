e9a5526e1b4500871078a11bbe4a769669489d58
Fomafix <fomafix@googlemail.com>
2025-01-13 21:21:22 +0000

    Use new service 'SyntaxHighlight.SyntaxHighlight'
    This change requires a Phan dependency on extension
    SyntaxHighlight_GeSHi added by change
    Icd697ac909fc2d3b98139cb6926b405191fb3f47.
    
    The Phan stub is not needed anymore.
    
    Depends-On: I7ae61fd8e7b10b723593b662f4f3b1d4cab00c21
    Change-Id: Ie58b372756b82f3093ed5408b1de2d2831636ce6

--- .phan/config.php
+++ .phan/config.php
@@ -2,6 +2,19 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
+$cfg['directory_list'] = array_merge(
+	$cfg['directory_list'],
+	[
+		'../SyntaxHighlight_GeSHi',
+	]
+);
+$cfg['exclude_analysis_directory_list'] = array_merge(
+	$cfg['exclude_analysis_directory_list'],
+	[
+		'../SyntaxHighlight_GeSHi',
+	]
+);
+
 $cfg['file_list'][] = 'EventLogging.namespaces.php';
 
 return $cfg;
